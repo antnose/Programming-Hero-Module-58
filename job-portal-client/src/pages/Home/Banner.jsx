b3533bce249f91386259c671980ee0e736e686fb
antnose <antnose28@gmail.com>
2025-03-18 21:57:32 +0600

    complete 8th video of module 58

--- job-portal-client/src/pages/Home/Banner.jsx
+++ job-portal-client/src/pages/Home/Banner.jsx
@@ -1,13 +1,24 @@
 import { easeInOut, motion } from "motion/react";
+import team1 from "../../assets/team/1.jpg";
+import team2 from "../../assets/team/2.jpg";
+import team3 from "../../assets/team/3.jpg";
 
 const Banner = () => {
   return (
     <div className="hero bg-base-200 min-h-96">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="flex-1">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-            className="max-w-sm rounded-lg shadow-2xl"
+          <motion.img
+            src={team1}
+            animate={{ y: [30, 90, 30] }}
+            transition={{ duration: 7, repeat: Infinity }}
+            className="max-w-sm w-64 rounded-t-[40px] rounded-br-[40px] border-l-4 border-b-4 border-amber-200 shadow-2xl"
+          />
+          <motion.img
+            src={team2}
+            animate={{ x: [100, 150, 100] }}
+            transition={{ duration: 7, repeat: Infinity }}
+            className="max-w-sm w-64 rounded-t-[40px] rounded-br-[40px] border-l-4 border-b-4 border-amber-200 shadow-2xl"
           />
         </div>
 
@@ -21,7 +32,16 @@ const Banner = () => {
             }}
             className="text-5xl font-bold"
           >
-            Latest Jobs For You!
+            Latest{" "}
+            <motion.span
+              animate={{
+                color: ["#ECFF33", "#b5db48", "#c2f72e"],
+              }}
+              transition={{ duration: 1.5, repeat: Infinity }}
+            >
+              Jobs
+            </motion.span>{" "}
+            For You!
           </motion.h1>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
