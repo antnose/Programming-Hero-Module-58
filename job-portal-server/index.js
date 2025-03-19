75ac4f6afc95ad55a6c2ed8b872b2d2881166ba8
antnose <antnose28@gmail.com>
2025-03-19 22:26:31 +0600

    complete 1st video of module 59

--- job-portal-server/index.js
+++ job-portal-server/index.js
@@ -1,11 +1,47 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3001;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// MongoDb Code Start
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.to58y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    app.get("/", (req, res) => {
+      res.send("Hello World");
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// MongoDb Code End
 
 app.get("/", (req, res) => {
   res.send("Hello World");
